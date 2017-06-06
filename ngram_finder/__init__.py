9bc6169adb8534e31edfee9237426942259cfbce
lovit <soy.lovit@gmail.com>
2017-06-06 19:24:36 +0900

    Commit Corpus

--- ngram_finder/__init__.py
+++ ngram_finder/__init__.py
@@ -2,7 +2,7 @@ __name__ = 'ngram_finder'
 __version__ = '0.0.1'
 __author__ = 'Lovit'
 
-from ._util import Corpus
+from ._util import DoublespaceLineCorpus
 from ._skipngram import SkipNgramFinder
 from ._simplengram import SimpleNgramFinder
 from ._score import cohesion_score
