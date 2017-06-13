4673f6fe0befe629f0c825b0f527b1be6e674cca
lovit <soy.lovit@gmail.com>
2017-06-14 02:59:52 +0900

    Rename util -> utils

--- ngram_finder/__init__.py
+++ ngram_finder/__init__.py
@@ -2,7 +2,7 @@ __name__ = 'ngram_finder'
 __version__ = '0.0.1'
 __author__ = 'Lovit'
 
-from .util import DoublespaceLineCorpus
+from .utils import DoublespaceLineCorpus
 from .skipngram import SkipNgramFinder
 from .simplengram import SimpleNgramFinder
 from .score import cohesion_score
